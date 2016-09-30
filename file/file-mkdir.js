1887de9caf98dcbd75f29b7d8d62a3f11fb32efe
huisoido <584331125@qq.com>
2016-09-30 13:26:42 +0800

    nodejs
    fs模块

---
+++ file/file-mkdir.js
@@ -0,0 +1,7 @@
+var fs = require("fs");
+
+console.log("创建目录 /hui");
+fs.mkdir("/hui", function(err){
+	if(err) return console.error(err);
+	console.log("目录创建成功。");
+});
--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 var fs = require("fs");
 
-//阻塞性
-var data = fs.readFileSync('input.txt');
-console.log(data.toString());
-console.log("阻塞性程序执行结束！");
-
 //非阻塞性
 fs.readFile('input.txt', function(err, data){
 	if(err) return console.error(err);
 	console.log(data.toString());
 });
 
-console.log("非阻塞性程序执行结束！");
+
+//阻塞性
+var data = fs.readFileSync('input.txt');
+console.log(data.toString());
+console.log("阻塞性程序执行结束！");
+
+
+console.log("程序执行结束！");
