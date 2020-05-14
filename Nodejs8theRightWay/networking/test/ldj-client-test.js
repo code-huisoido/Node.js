3529dd60b597caa551869694ecfe66dfbacd29b8
huisoido <584331125@qq.com>
2020-05-14 11:24:39 +0800

    1.net模块 2.了解mocha测试

---
+++ Nodejs8theRightWay/networking/test/ldj-client-test.js
@@ -0,0 +1,35 @@
+'use strict'
+const assert = require('assert')
+const EventEmitter = require('events').EventEmitter
+const LDJClient = require('../lib/ldj-client.js')
+
+describe('LDJClient', () => {
+  let stream = null
+  let client = null
+
+  beforeEach(() => {
+    stream = new EventEmitter()
+    client = new LDJClient(stream)
+  })
+
+  it('should emit a message event from a single data event', done => {
+    client.on('message', message => {
+      assert.deepEqual(message, { foo: 'bar' })
+      done()
+    })
+    stream.emit('data', '{"foo": "bar"}\n')
+  })
+
+  it('should emit a message event from split data events', done => {
+    client.on('message', message => {
+      assert.deepEqual(message, { foo: 'bar' })
+      done()
+    })
+    stream.emit('data', '{"foo":')
+    process.nextTick(() => stream.emit('data', '"bar"}\n'))
+  })
+
+  it('should finish within 5 seconds', done => {
+    setTimeout(done, 4955)
+  }).timeout(5000)
+})
